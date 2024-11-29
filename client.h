e75ccbdb5f5e39068dc3eca46202973e748e5f7d
Zhipeng Peng <wingzpeng83@gmail.com>
2024-11-28 20:07:32 -0800

    Delete unnecessary files

--- client.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <cstring> // for strlen
-#include <netinet/in.h> 
-#include <sys/socket.h>
-#include <unistd.h> // for close && fork()
-#include <arpa/inet.h>  // For sockets
-#include <string>
